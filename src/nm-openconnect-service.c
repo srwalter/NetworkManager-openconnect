enen92/nm-oc

commit e19552951357ffce9379e68ae00329550589c54e

service: include --csd-wrapper script in openconnect arguments
At least one protocol (GlobalProtect) requires the security checker ("CSD")
script to be invoked during the tunnel/connection phase, rather than the
authentication phase, because the access-enabling mechanism depends on
knowing the IP address(es) of the client in the internal network.  See
check_or_submit_hip_report() in cstp.c in OpenConnect.

src/nm-openconnect-service.c
@@ -394,6 +394,7 @@ nm_openconnect_start_openconnect_binary (NMOpenconnectPlugin *plugin,
 	GSource *openconnect_watch;
 	gint	stdin_fd;
 	const char *props_vpn_gw, *props_cookie, *props_cacert, *props_mtu, *props_gwcert, *props_proxy;
+	const char *props_csd_enable, *props_csd_wrapper;
 	const char *protocol;
 
 	/* Find openconnect */
@@ -478,6 +479,18 @@ nm_openconnect_start_openconnect_binary (NMOpenconnectPlugin *plugin,
 
 	g_ptr_array_add (openconnect_argv, (gpointer) "--script");
 	g_ptr_array_add (openconnect_argv, (gpointer) NM_OPENCONNECT_HELPER_PATH);
+
+	props_csd_enable = nm_setting_vpn_get_data_item (s_vpn, NM_OPENCONNECT_KEY_CSD_ENABLE);
+	props_csd_wrapper = nm_setting_vpn_get_data_item (s_vpn, NM_OPENCONNECT_KEY_CSD_WRAPPER);
+	if (props_csd_enable && !strcmp (props_csd_enable, "yes") && props_csd_wrapper) {
+		/* Replicate the CSD parameters used in the authentication phase, for
+		   supported protocols which may need to invoke the security trojan ("CSD")
+		   in the tunnel/connection phase. */
+		g_ptr_array_add (openconnect_argv, (gpointer) "--csd-wrapper");
+		g_ptr_array_add (openconnect_argv, (gpointer) props_csd_wrapper);
+		g_ptr_array_add (openconnect_argv, (gpointer) "--csd-user");
+		g_ptr_array_add (openconnect_argv, (gpointer) g_strdup_printf ("%d", gl.tun_owner));
+	}
 
 	priv->tun_name = create_persistent_tundev ();
 	if (priv->tun_name) {

srwalter/NetworkManager-openconnect

commit 632eb5b3b1f0abb2811ac53dcd9c9f78cfa792c9

service: only call csd-wrapper if we are also dropping priviledges
Otherwise, it's not safe.

Fixes: e19552951357ffce9379e68ae00329550589c54e

src/nm-openconnect-service.c
@@ -481,22 +481,27 @@ nm_openconnect_start_openconnect_binary (NMOpenconnectPlugin *plugin,
 	g_ptr_array_add (openconnect_argv, (gpointer) "--script");
 	g_ptr_array_add (openconnect_argv, (gpointer) NM_OPENCONNECT_HELPER_PATH);
 
-	props_csd_enable = nm_setting_vpn_get_data_item (s_vpn, NM_OPENCONNECT_KEY_CSD_ENABLE);
-	props_csd_wrapper = nm_setting_vpn_get_data_item (s_vpn, NM_OPENCONNECT_KEY_CSD_WRAPPER);
-	if (props_csd_enable && !strcmp (props_csd_enable, "yes") && props_csd_wrapper) {
-		/* Replicate the CSD parameters used in the authentication phase, for
-		   supported protocols which may need to invoke the security trojan ("CSD")
-		   in the tunnel/connection phase. */
-		g_ptr_array_add (openconnect_argv, (gpointer) "--csd-wrapper");
-		g_ptr_array_add (openconnect_argv, (gpointer) props_csd_wrapper);
-		g_ptr_array_add (openconnect_argv, (gpointer) "--csd-user");
-		g_ptr_array_add (openconnect_argv, (gpointer) nm_sprintf_buf (csd_user_arg, "%d", gl.tun_owner));
-	}
-
 	priv->tun_name = create_persistent_tundev ();
 	if (priv->tun_name) {
 		g_ptr_array_add (openconnect_argv, (gpointer) "--interface");
 		g_ptr_array_add (openconnect_argv, (gpointer) priv->tun_name);
+	}
+
+	props_csd_enable = nm_setting_vpn_get_data_item (s_vpn, NM_OPENCONNECT_KEY_CSD_ENABLE);
+	props_csd_wrapper = nm_setting_vpn_get_data_item (s_vpn, NM_OPENCONNECT_KEY_CSD_WRAPPER);
+	if (props_csd_enable && !strcmp (props_csd_enable, "yes") && props_csd_wrapper) {
+		if (priv->tun_name) {
+			/* Replicate the CSD parameters used in the authentication phase, for
+			   supported protocols which may need to invoke the security trojan ("CSD")
+			   in the tunnel/connection phase. */
+			g_ptr_array_add (openconnect_argv, (gpointer) "--csd-wrapper");
+			g_ptr_array_add (openconnect_argv, (gpointer) props_csd_wrapper);
+			g_ptr_array_add (openconnect_argv, (gpointer) "--csd-user");
+			g_ptr_array_add (openconnect_argv, (gpointer) nm_sprintf_buf (csd_user_arg, "%d", gl.tun_owner));
+		} else {
+			_LOGW ("openconnect won't call csd-wrapper script because it cannot drop privileges to user \"%s\"",
+			       NM_OPENCONNECT_USER);
+		}
 	}
 
 	g_ptr_array_add (openconnect_argv, (gpointer) props_vpn_gw);