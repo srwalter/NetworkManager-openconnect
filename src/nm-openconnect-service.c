enen92/nm-oc

commit e19552951357ffce9379e68ae00329550589c54e

service: include --csd-wrapper script in openconnect arguments
At least one protocol (GlobalProtect) requires the security checker ("CSD")
script to be invoked during the tunnel/connection phase, rather than the
authentication phase, because the access-enabling mechanism depends on
knowing the IP address(es) of the client in the internal network.  See
check_or_submit_hip_report() in cstp.c in OpenConnect.

src/nm-openconnect-service.c
@@ -394,6 +394,7 @@ nm_openconnect_start_openconnect_binary (NMOpenconnectPlugin *plugin,
 	GSource *openconnect_watch;
 	gint	stdin_fd;
 	const char *props_vpn_gw, *props_cookie, *props_cacert, *props_mtu, *props_gwcert, *props_proxy;
+	const char *props_csd_enable, *props_csd_wrapper;
 	const char *protocol;
 
 	/* Find openconnect */
@@ -478,6 +479,18 @@ nm_openconnect_start_openconnect_binary (NMOpenconnectPlugin *plugin,
 
 	g_ptr_array_add (openconnect_argv, (gpointer) "--script");
 	g_ptr_array_add (openconnect_argv, (gpointer) NM_OPENCONNECT_HELPER_PATH);
+
+	props_csd_enable = nm_setting_vpn_get_data_item (s_vpn, NM_OPENCONNECT_KEY_CSD_ENABLE);
+	props_csd_wrapper = nm_setting_vpn_get_data_item (s_vpn, NM_OPENCONNECT_KEY_CSD_WRAPPER);
+	if (props_csd_enable && !strcmp (props_csd_enable, "yes") && props_csd_wrapper) {
+		/* Replicate the CSD parameters used in the authentication phase, for
+		   supported protocols which may need to invoke the security trojan ("CSD")
+		   in the tunnel/connection phase. */
+		g_ptr_array_add (openconnect_argv, (gpointer) "--csd-wrapper");
+		g_ptr_array_add (openconnect_argv, (gpointer) props_csd_wrapper);
+		g_ptr_array_add (openconnect_argv, (gpointer) "--csd-user");
+		g_ptr_array_add (openconnect_argv, (gpointer) g_strdup_printf ("%d", gl.tun_owner));
+	}
 
 	priv->tun_name = create_persistent_tundev ();
 	if (priv->tun_name) {

srwalter/NetworkManager-openconnect

commit 92dcce45302c6dcfcb43cdede6f0f3fe36583677

service: fix leak constructing command line args for csd-user
Fixes: e19552951357ffce9379e68ae00329550589c54e

src/nm-openconnect-service.c
@@ -388,11 +388,12 @@ nm_openconnect_start_openconnect_binary (NMOpenconnectPlugin *plugin,
                                          GError **error)
 {
 	NMOpenconnectPluginPrivate *priv = NM_OPENCONNECT_PLUGIN_GET_PRIVATE (plugin);
-	GPid	pid;
+	GPid pid;
 	const char **openconnect_binary = NULL;
 	GPtrArray *openconnect_argv;
 	GSource *openconnect_watch;
-	gint	stdin_fd;
+	gint stdin_fd;
+	char csd_user_arg[60];
 	const char *props_vpn_gw, *props_cookie, *props_cacert, *props_mtu, *props_gwcert, *props_proxy;
 	const char *props_csd_enable, *props_csd_wrapper;
 	const char *protocol;
@@ -489,7 +490,7 @@ nm_openconnect_start_openconnect_binary (NMOpenconnectPlugin *plugin,
 		g_ptr_array_add (openconnect_argv, (gpointer) "--csd-wrapper");
 		g_ptr_array_add (openconnect_argv, (gpointer) props_csd_wrapper);
 		g_ptr_array_add (openconnect_argv, (gpointer) "--csd-user");
-		g_ptr_array_add (openconnect_argv, (gpointer) g_strdup_printf ("%d", gl.tun_owner));
+		g_ptr_array_add (openconnect_argv, (gpointer) nm_sprintf_buf (csd_user_arg, "%d", gl.tun_owner));
 	}
 
 	priv->tun_name = create_persistent_tundev ();